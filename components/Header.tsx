df13ab4c34b9935fe80f5277e0985c55c48feb5d
Matthew <glowupmatt@gmail.com>
2023-05-26 16:29:41 -0700

    changes to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -19,32 +19,38 @@ const Header: React.FC<HeaderProps> = ({ onClick }) => {
 
   return (
     <div className=" flex flex-col lg:flex-row">
-      <header className=" flex bg-white p-7 justify-center w-full align-center">
-        <div className="flex justify-between w-full">
-          <Image
-            alt="logo"
-            src="/images/logo.svg"
-            width={120.04}
-            height={20.67}
-            className="mr-auto self-center h-8"
-          />
+      <header
+        className=" flex bg-white p-7 justify-center w-full align-center
 
-          <img
-            alt=""
-            src="/images/bars-solid.svg"
-            className="ml-auto block w-[25px] lg:items-center lg:justify-end lg:hidden z-90 lg:w-0 lg:h-0 lg:invisible"
-            onClick={handleClick}
-          />
-        </div>
-        <div
-          className={clsx(
-            "z-[1000] lg:opacity-100 w-full flex absolute lg:items-end lg:relative justify-center top-[5.5rem] lg:bottom-auto",
-            !showNav && "opacity-0 z-50 w-0 h-0 "
-          )}
-        >
-          <Nav showNav={showNav} />
-        </div>
+    "
+      >
+        <Image
+          alt="logo"
+          src="/images/logo.svg"
+          width={120.04}
+          height={20.67}
+          className="mr-auto self-center h-8"
+        />
+        <MenuIcon
+          onClick={handleClick}
+          className="
+          ml-auto
+          lg:hidden
+          sm:z-90
+          lg:w-0
+          lg:h-0
+          lg:invisible
+      "
+        />
       </header>
+      <div
+        className={clsx(
+          "z-[1000] lg:opacity-100  w-full flex absolute lg:relative justify-center bottom-40 lg:bottom-auto",
+          !showNav && "opacity-0 z-50 w-0 h-0 "
+        )}
+      >
+        <Nav showNav={showNav} />
+      </div>
     </div>
   );
 };
